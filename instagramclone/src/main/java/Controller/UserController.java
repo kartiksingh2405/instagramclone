821750a46cdb8b7d20e31404f434728c4175d89a
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-27 17:10:31 +0530

    post mapping

--- instagramclone/src/main/java/Controller/UserController.java
+++ instagramclone/src/main/java/Controller/UserController.java
@@ -1,9 +1,17 @@
 package Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @PostMapping("")
+    private boolean submitUser() {
+        return true;
+    }
+
+    
 }
