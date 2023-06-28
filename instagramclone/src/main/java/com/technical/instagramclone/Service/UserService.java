af7a3b81c3d55545fae76f58299a1955bffda5e5
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-28 16:31:10 +0530

    created service

---
+++ instagramclone/src/main/java/com/technical/instagramclone/Service/UserService.java
@@ -0,0 +1,16 @@
+package com.technical.instagramclone.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private Users submitMetaDataOfUser(Users user) {
+        return new Users();
+    }
+
+    private Users displayUserMetaData(String userid) {
+        return new Users();
+    }
+
+}
