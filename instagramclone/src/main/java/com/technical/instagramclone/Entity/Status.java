1ef611d169101454cc7ca854e0f69bd7ff774daf
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-29 15:39:40 +0530

    update status

--- instagramclone/src/main/java/com/technical/instagramclone/Entity/Status.java
+++ instagramclone/src/main/java/com/technical/instagramclone/Entity/Status.java
@@ -1,7 +1,73 @@
 package com.technical.instagramclone.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.sql.Timestamp;
 
 @Entity(name="Status")
 public class Status {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    
+    private String statusId;
+    private String userId;
+    private String path;
+    private Timestamp timestamp;
+
+    public Status() {
+        super();
+    }
+
+    public Status(int id, String statusId, String userId, String path, Timestamp timestamp) {
+        this.id = id;
+        this.statusId = statusId;
+        this.userId = userId;
+        this.path = path;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(String statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }
