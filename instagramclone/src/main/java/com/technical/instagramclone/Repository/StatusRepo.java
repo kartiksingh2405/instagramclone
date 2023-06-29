55ad485b7fe76154429c8622a103a7b59a1abe2e
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-29 16:05:50 +0530

    Staus repo

--- instagramclone/src/main/java/com/technical/instagramclone/Repository/StatusRepo.java
+++ instagramclone/src/main/java/com/technical/instagramclone/Repository/StatusRepo.java
@@ -1,4 +1,15 @@
 package com.technical.instagramclone.Repository;
 
-public interface StatusRepo {
+import com.technical.instagramclone.Entity.Status;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+
+public interface StatusRepo extends CrudRepository<Status, Integer> {
+
+    Status save(Status save);
+    ArrayList<Status> findAll(); 
 }
