12c02e74e37235873e555baba3ab47db2d10da6a
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-30 17:03:09 +0530

    Post Conroller

--- instagramclone/src/main/java/com/technical/instagramclone/Controller/PostController.java
+++ instagramclone/src/main/java/com/technical/instagramclone/Controller/PostController.java
@@ -1,4 +1,21 @@
 package com.technical.instagramclone.Controller;
 
+import com.technical.instagramclone.Entity.Post;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/post")
 public class PostController {
+
+    @PostMapping("")
+    private Post submitUserPost(@RequestBody Post post){
+        return new Post();
+    }
+
+    @GetMapping("/post")
+    private ArrayList<Post> getAllPost(){
+        return new ArrayList<Post>();
+    }
 }
