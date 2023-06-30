00293655e8ab22de108d32ad03931e95c02f32c1
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-30 17:10:30 +0530

    update PostController

--- instagramclone/src/main/java/com/technical/instagramclone/Controller/PostController.java
+++ instagramclone/src/main/java/com/technical/instagramclone/Controller/PostController.java
@@ -1,6 +1,8 @@
 package com.technical.instagramclone.Controller;
 
 import com.technical.instagramclone.Entity.Post;
+import com.technical.instagramclone.Service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,13 +11,16 @@ import java.util.ArrayList;
 @RequestMapping("/post")
 public class PostController {
 
+    @Autowired
+    PostService postService;
+
     @PostMapping("")
     private Post submitUserPost(@RequestBody Post post){
-        return new Post();
+        return postService.submitPostToDataBase(post);
     }
 
     @GetMapping("/post")
     private ArrayList<Post> getAllPost(){
-        return new ArrayList<Post>();
+        return postService.retrievePostFromDB();
     }
 }
