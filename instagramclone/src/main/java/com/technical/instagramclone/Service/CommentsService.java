96419cede328a151e591cc706dbc141cc29392e8
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-07-01 17:03:53 +0530

    CommentsService

---
+++ instagramclone/src/main/java/com/technical/instagramclone/Service/CommentsService.java
@@ -0,0 +1,19 @@
+package com.technical.instagramclone.Service;
+
+import com.technical.instagramclone.Entity.Comments;
+import com.technical.instagramclone.Entity.Users;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CommentsService {
+
+    public Comments submitCommentToDB(Comments comment) {
+    }
+
+    public ArrayList<Comments> getAllCommentToDB(String postId){
+
+    }
+
+}
